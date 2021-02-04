9df2982fbf19b9297f625bb2e703fc56b1e69bf3
slowy07 <slowy.arfy@gmail.com>
2021-02-05 06:18:40 +0700

    profile image

---
+++ lib/util/friends.dart
@@ -0,0 +1,11 @@
+List friends = [
+  "assets/anastasia.jpg",
+  "assets/dasha.jpg",
+  "assets/geyonujung.jpg",
+  "assets/somi.jpg",
+  "assets/dasha.jpg",
+  "assets/geyonujung.jpg",
+  "assets/somi.jpg",
+  "assets/geyonujung.jpg",
+  "assets/anastasia.jpg"
+];
